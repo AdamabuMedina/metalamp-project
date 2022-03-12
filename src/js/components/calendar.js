38213777f2aed8089f4a9759ad3cd035076c6326
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-03-12 20:05:53 +0300

    стилизация календаря

--- src/js/components/calendar.js
+++ src/js/components/calendar.js
@@ -4,5 +4,19 @@ const dateInput = ".calendar-input"
 
 $(dateInput).datepicker({
   clearButton: true,
-  todayButton: new Date(),
+  todayButton: true,
+  multipleDates: 2,
+  // minDate: new Date(),
+  toggleSelected: false,
+  offset: 5,
+  range: true,
+  prevHtml: '<svg><path d="M 13,10 l -7,7 l 7,7"></path><path d="M 6,17 l 16,0"></path></svg>',
+  nextHtml: '<svg><path d="M 19,10 l 7,7 l -7,7"></path><path d="M 26,17 l -16,0"></path></svg>',
+  language: {
+    today: 'Применить',
+  },
+  navTitles: {
+    days: 'MM yyyy',
+  },
+  // classes: "cards-number--five"
 })
